28e418101d3ed304cdaf382abc76f2a2c2492ba1
Chris-Saints <christianpicoli18@gmail.com>
2025-11-28 14:31:15 -0300

    Criação do componente Banner Criação do componente Tema Criação do componente CardEvento Adição das imagens que srão utilizadas no projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,48 @@
 import './App.css'
+import { Banner } from './components/Banner'
+import { CardEvento } from './components/CardEvento'
 import { FormularioDeEvento } from './components/FormularioDeEventos'
+import { Tema } from './components/Tema'
 
 export function App() {
+
+  const temas = [
+    {
+      id: 1,
+      nome: 'front-end'
+    },
+    {
+      id: 2,
+      nome: 'back-end'
+    },
+    {
+      id: 3,
+      nome: 'devops'
+    },
+    {
+      id: 4,
+      nome: 'inteligência artificial'
+    },
+    {
+      id: 5,
+      nome: 'data science'
+    },
+    {
+      id: 6,
+      nome: 'cloud'
+    }
+  ]
+
+  const eventos = [
+    {
+      capa: '../public/Imagem1.svg',
+      tema: temas[0],
+      data: new Date(),
+      titulo: 'Mulheres no front',
+      descricao: 'Valorizando e impulsionando a participação feminina no desenvolvimento front-end.'
+    }
+  ]
+
 
   return (
     <main>
@@ -9,12 +50,19 @@ export function App() {
         <img src="/logo.png" alt="" />
       </header>
 
-      <section>
-        <img src="/banner.png" alt="" />
-      </section>
+      <Banner />
 
       <FormularioDeEvento />
 
+      {temas.map(function (item) {
+        return (
+          <section key={item.id}>
+            <Tema tema={item}/>
+
+            <CardEvento evento={eventos[0]} />
+          </section>
+        )
+      })} 
     </main>
   )
 }
